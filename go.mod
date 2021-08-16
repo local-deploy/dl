e9943e9da6ff5e60fcf47fb93d52dab8337c2106
Sergey Voloshin <git@varme.pw>
2021-08-17 00:42:09 +0300

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/varrcan/dl
+
+go 1.15
+
+require (
+	github.com/containerd/containerd v1.5.5 // indirect
+	github.com/docker/docker v20.10.8+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/joho/godotenv v1.3.0
+	google.golang.org/grpc v1.40.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+var colorRed = "\033[31m"
+var colorGreen = "\033[32m"
+
+func init() {
+	// загрузить переменные из .env
+	if err := godotenv.Load(); err != nil {
+		log.Println(colorRed, "No .env file found")
+		os.Exit(1)
+	}
+}
+
+func main() {
+	composeProjectName := getProjectName()
+
+	log.Println(colorGreen, composeProjectName)
+}
+
+func getProjectName() string {
+	composeProjectName, exists := os.LookupEnv("COMPOSE_PROJECT_NAME")
+
+	if !exists {
+		fmt.Println(colorRed, "Key COMPOSE_PROJECT_NAME not found in .env file")
+		os.Exit(1)
+	}
+
+	return composeProjectName
+}
