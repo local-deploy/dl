e1b87d3f20b972c13b3bc81e284762e94efc2e45
Sergey Voloshin <git@varme.pw>
2022-10-23 21:37:51 +0300

    feat(app): version up

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/varrcan/dl/helper"
 )
 
-var version = "0.4.5"
+var version = "0.4.6"
 
 func main() {
 	pterm.ThemeDefault.SecondaryStyle = pterm.Style{pterm.FgDefault, pterm.BgDefault}
